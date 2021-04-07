7fcd94d901b49be7a7beee3d81f1a142e35de9fe
George <gstclair66@gmail.com>
2021-04-06 21:12:43 -0500

    added type hints to da8 assignment

--- da8.py
+++ da8.py
@@ -1,16 +1,16 @@
 import random
 
-def fourBitStrings(n):
+def fourBitStrings(n: int) -> None:
     with open('fourBits.txt', 'w') as fl:
         for i in range(n):
             fl.write(format(random.randint(1,16),'04b') + '\n')
 
-def makeStrings(numbits = 4, numstrings = 10, fl = 'myFile.txt'):
+def makeStrings(numbits: int = 4, numstrings: int = 10, fl = 'myFile.txt') -> None:
     with open(fl, 'w') as outfile:
         for i in range(numstrings):
             outfile.write(format(random.randint(1,2**numbits),'0' + str(numbits) + 'b') + '\n')
 
-def eightBits(n=8):
+def eightBits(n: int=8) -> None:
     if n > 256 or n < 0:
         raise ValueError('Error: value out of range')
     with open('nBits.txt','w') as fl:
